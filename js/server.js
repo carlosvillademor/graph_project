251063c560ff8d3b4c67e0c3cbbe1faaf441c155
carlosvillademor <carlos.villademor@gmail.com>
2013-04-30 06:39:41 +0100

    Sending a response back for the test route

--- js/server.js
+++ js/server.js
@@ -1,8 +1,9 @@
 var express = require('express'),
 	app = express();
 
-app.get('/test', function() {
+app.get('/test', function(req, res) {
 	console.log('test method, called received by express server on /test');
+	res.send('OK');
 });
 
 app.listen(3000);
