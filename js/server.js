c97d34721f665c6b989b001e4b5245e01385de07
carlosvillademor <carlos.villademor@gmail.com>
2013-04-30 23:03:02 +0100

    Adding route for get request to /

--- js/server.js
+++ js/server.js
@@ -3,7 +3,12 @@ var express = require('express'),
 
 app.get('/test', function(req, res) {
 	console.log('test method, called received by express server on /test');
-	res.send('OK');
+	res.send('get request to /test');
+});
+
+app.get('/', function(req, res) {
+	console.log('get request to /');
+	res.send('get request to /');
 });
 
 app.listen(30000);
