97e43c7c09136ff87468e58cf6b5d83b24719065
carlosvillademor <carlos.villademor@gmail.com>
2013-05-11 00:44:07 +0100

    adding login link

--- js/server.js
+++ js/server.js
@@ -1,14 +1,27 @@
 var express = require('express'),
-	app = express();
+	app = express(),
+	mu = require('mu2'),
+	util = require('util');
+
+mu.root = __dirname;
 
 app.get('/test', function(req, res) {
 	console.log('test method, called received by express server on /test');
-	res.send('get request to /test');
+	res.send('Test page');
+});
+
+app.get('/channel.html', function(req, res){
+	console.log('get request to /channel.html');
+	var channelPage = mu.compileAndRender('../templates/channel.html', {});
+	console.log('channelPage is', channelPage);
+	util.pump(channelPage, res);
 });
 
 app.get('/', function(req, res) {
 	console.log('get request to /');
-	res.send('get request to /');
+	var homePage = mu.compileAndRender('../templates/home.html', {});
+	console.log('homePage is', homePage);
+	util.pump(homePage, res);
 });
 
 app.listen(process.env.PORT || 5000);
